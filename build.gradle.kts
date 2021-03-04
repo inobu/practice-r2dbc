59aa12e043f68aec6155c039a4a8ffc9d42ec116
inobu <inohkunobuya@gmail.com>
2021-03-05 00:30:43 +0900

    JibDockerBuildを追加する

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "2.3.1.RELEASE"
 	id("io.spring.dependency-management") version "1.0.9.RELEASE"
+	id("com.google.cloud.tools.jib") version "2.8.0"
 	kotlin("jvm") version "1.3.72"
 	kotlin("plugin.spring") version "1.3.72"
 }
